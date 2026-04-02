38a8c9022f55e8718b332bf82ff7c8a4cb903bce
tapeshchavle <tapeshchawle@gmail.com>
2026-04-03 00:17:13 +0530

    files added

---
+++ src/main/java/com/test/test_one/TestOneApplication.java
@@ -0,0 +1,13 @@
+package com.test.test_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/test/test_one/controller/GreetController.java
@@ -0,0 +1,15 @@
+package com.test.test_one.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/greet")
+public class GreetController {
+
+    @GetMapping
+    public String greet(){
+        return "Hello Tapesh from Azure";
+    }
+}
