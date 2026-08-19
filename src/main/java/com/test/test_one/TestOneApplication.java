39472cdac4ebed704623fdf284c8058489262933
tapeshchavle <tapeshchawle@gmail.com>
2026-08-19 17:25:27 +0530

    fixed

--- src/main/java/com/test/test_one/TestOneApplication.java
+++ src/main/java/com/test/test_one/TestOneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestOneApplication {
-
+ // testing all
 	public static void main(String[] args) {
 		SpringApplication.run(TestOneApplication.class, args);
 	}
